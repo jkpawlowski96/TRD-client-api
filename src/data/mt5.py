401c4a14da19f0019dec4f2f9accba88041cc484
jkpawlowski96 <jkpawlowski96@gmail.com>
2020-10-10 22:49:31 +0200

    mt4 price data get

--- src/data/mt5.py
+++ src/data/mt5.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 from src.data.api import DataApi
- 
+from src.models import Interval, Pair
+import MetaTrader5 as mt5
+
 class MT5(DataApi):
+
+    intervals = {'m1': mt5.TIMEFRAME_M1}
+
     def __init__(self):
         # connect to MetaTrader 5
-        import MetaTrader5 as mt5
         if not mt5.initialize():
             print("initialize() failed")
             mt5.shutdown()
@@ -13,5 +17,26 @@ class MT5(DataApi):
         print(mt5.terminal_info())
         # get data on MetaTrader 5 version
         print(mt5.version())
-    
-    
+
+    def get_pair_price(self,  
+                       _datetime:datetime, 
+                       interval:Interval,
+                       pair:Pair):
+
+        rates = mt5.copy_rates_from(pair.get_name(), 
+                                    self.intervals[interval.get_name()],
+                                    _datetime, 1000)
+        rates = rates.tolist()
+        return rates
+
+    def get_pair_range_price(self, 
+                             datetime_start:datetime, 
+                             datetime_end:datetime, 
+                             interval:Interval,
+                             pair:Pair):
+
+        rates = mt5.copy_rates_range(pair.get_name(), 
+                                    self.intervals[interval.get_name()],
+                                    datetime_start, datetime_end)
+        rates = rates.tolist()
+        return rates
--- src/models/interval.py
+++ src/models/interval.py
@@ -1,2 +1,19 @@
 class Interval:
-    pass
+
+    type_name = {0:'tick',
+                 1:'m1',
+                 2:'m5'}
+    
+    name_type = {v:k for k,v in type_name.items()}
+    names = list(name_type.keys())
+
+    def __init__(self, value='m1'):
+        if value not in self.names:
+            return AssertionError, 'Interval value not exists'
+        self.type = self.name_type[value]
+
+    def get_name(self):
+        return self.type_name[self.type]
+    
+    def get_type(self):
+        return self.type
