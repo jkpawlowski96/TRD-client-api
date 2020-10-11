e8850849db7e8ac1508e4b8f75f75494e6ba5abd
jkpawlowski96 <jkpawlowski96@gmail.com>
2020-10-11 14:30:27 +0200

    history and history_range api

--- src/data/data.py
+++ src/data/data.py
@@ -1,12 +1,32 @@
+from src.models.interval import Interval
 from src.data import DataApi, MT5
+from src.models import Pair
+from datetime import datetime
 
 class Data:
     def __init__(self):
         self.mt5 = MT5()
 
+    def history(self, pair:Pair, datetime_start:int, datetime_end:int=None, interval:Interval=None):
+        api = self.mt5
+        datetime_start = datetime.fromtimestamp(datetime_start)
+        if not interval:
+            interval = Interval('m1')
+        if datetime_end:
+            # range
+            datetime_end = datetime.fromtimestamp(datetime_end)
+            res = api.get_pair_range_price( pair=pair, 
+                                            datetime_start=datetime_start, 
+                                            datetime_end=datetime_end, 
+                                            interval=interval)
+        else:
+            # single 
+            res = api.get_pair_price(pair=pair, _datetime=datetime_start, interval=interval)
+        return res
+
     def test(self):
         res = []
         for api in [self.mt5]:
-            _res = {str(api.__class__):self.mt5.test()} 
+            _res = {'mt5':self.mt5.test()} 
             res.append(_res)
         return res
--- src/data/mt5.py
+++ src/data/mt5.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from src.data.api import DataApi
 from src.models import Interval, Pair
 import MetaTrader5 as mt5
+import numpy as np
 
 class MT5(DataApi):
 
@@ -18,25 +19,34 @@ class MT5(DataApi):
         # get data on MetaTrader 5 version
         print(mt5.version())
 
-    def get_pair_price(self,  
+    def get_pair_price(self,
+                       pair:Pair,  
                        _datetime:datetime, 
-                       interval:Interval,
-                       pair:Pair):
+                       interval:Interval):
 
         rates = mt5.copy_rates_from(pair.get_name(), 
                                     self.intervals[interval.get_name()],
                                     _datetime, 1000)
-        rates = rates.tolist()
+        rates = price_extractor(rates[0])
         return rates
 
     def get_pair_range_price(self, 
+                             pair:Pair,   
                              datetime_start:datetime, 
                              datetime_end:datetime, 
-                             interval:Interval,
-                             pair:Pair):
+                             interval:Interval):
 
         rates = mt5.copy_rates_range(pair.get_name(), 
                                     self.intervals[interval.get_name()],
                                     datetime_start, datetime_end)
-        rates = rates.tolist()
+        
+        rates = [price_extractor(row) for row in rates]
         return rates
+
+def price_extractor(arr):
+    return [arr[1], # open
+            arr[2], # high
+            arr[3], # low
+            arr[4], # close
+            float(arr[6]) # spread
+            ]
